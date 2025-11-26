4100272e73014e5d4fd8b79e9092ae2c3d03efdf
Korczek <105612166+Korczek@users.noreply.github.com>
2025-11-26 21:58:26 +0100

    structures

--- Auth.Api/Program.cs
+++ Auth.Api/Program.cs
@@ -1,3 +1,7 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 namespace Auth;
 
 public class Program
@@ -5,9 +9,38 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // warn: To docelowo na być wczytywane z sekretów aws / dodtnet lokal files czy coś takiego 
+        var jwt = builder.Configuration.GetSection("Jwt"); 
+
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = jwt["Issuer"],
+
+                    ValidateAudience = true,
+                    ValidAudience = jwt["Audience"],
+
+                    ValidateLifetime = true,
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt["Key"]!))
+                };
+            });
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddControllers();
+        
         var app = builder.Build();
 
-        app.MapGet("/", () => "Hello World!");
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.MapControllers();
+
+        app.MapGet("/Auth", () => "Hello in Auth.Api");
 
         app.Run();
     }
